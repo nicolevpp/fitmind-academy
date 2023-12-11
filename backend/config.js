ab856de2f1cecddf3836ee8ec1dc8ca25e540a9d
Nicole Perez <nicoleperezdigital@gmail.com>
2023-12-11 10:46:27 -0400

    testing building

--- backend/config.js
+++ backend/config.js
@@ -1,10 +1,10 @@
 const PAGE_URL = process.env.NODE_ENV === 'production'
-? 'placeholder'
-: 'http://localhost:3005'
+  ? 'http://dashboard.fitmind-academy.com'
+  : 'http://localhost:3003';
 
 const MONGO_URI = process.env.NODE_ENV === 'production'
-? process.env.MONGO_URI_PROD
-: process.env.MONGO_URI_TEST
+  ? process.env.MONGO_URI_PROD
+  : process.env.MONGO_URI_TEST;
 
 
 module.exports = { PAGE_URL, MONGO_URI };
--- backend/models/user.js
+++ backend/models/user.js
@@ -2,28 +2,28 @@ const mongoose = require('mongoose');
 // passwordHash: String,
 
 const userSchema = new mongoose.Schema({
-    firstName: String,
-    lastName: String,
-    email: String,
-    phone: String,
-    address: String,
-    password: String,
-    admin: {
-        type: Boolean,
-        default: false
-    },
-    routines: [{
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'Routines'
-    }]
+  firstName: String,
+  lastName: String,
+  email: String,
+  phone: String,
+  address: String,
+  password: String,
+  admin: {
+    type: Boolean,
+    default: false
+  },
+  routines: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Routines'
+  }]
 });
 
 userSchema.set('toJSON', {
-transform: (document, returnedObject) => {
+  transform: (document, returnedObject) => {
     returnedObject.id = returnedObject._id.toString();
     delete returnedObject._id;
     delete returnedObject.__v;
-}
+  }
 });
 
 const User = mongoose.model('User', userSchema);
